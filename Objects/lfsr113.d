6e35764eeb0eb2bab984e28ef8156b99e74fdbe1
Andrew Drury <drurya99@gmail.com>
2019-07-17 00:25:00 -0400

    Added libs and needed files

---
+++ Objects/lfsr113.d
@@ -0,0 +1,3 @@
+.\objects\lfsr113.o: lfsr113.c
+.\objects\lfsr113.o: lfsr113.h
+.\objects\lfsr113.o: C:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ Objects/random.d
@@ -0,0 +1,4 @@
+.\objects\random.o: random.c
+.\objects\random.o: random.h
+.\objects\random.o: C:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\random.o: lfsr113.h
